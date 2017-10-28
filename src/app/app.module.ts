ab67e4b821d6af8dccd51ff17d4c09fcb1b65e1e
quace <quinten.moesen@gmail.com>
2017-10-28 17:18:42 +0200

    Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
+import { UserService } from './user.service';
 
 import { AppComponent } from './app.component';
+
+export function getAuthHttp(http: Http) {
+  return new AuthHttp(new AuthConfig({
+    headerName: 'x-auth-token',
+    noTokenScheme: true,
+    noJwtError: true,
+    globalHeaders: [{'Accept': 'application/json'}],
+    tokenGetter: (() => localStorage.getItem('id_token')),
+  }), http);
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +27,14 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+    {
+      provide: AuthHttp,
+      useFactory: getAuthHttp,
+      deps: [Http]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
